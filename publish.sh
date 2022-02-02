8d6aa17117c05916f8f208202290079efee5bd9b
litnimax <litnimaxster@gmail.com>
2022-02-02 11:40:38 +0300

    Removed CNAME

--- publish.sh
+++ publish.sh
@@ -7,7 +7,7 @@ rm -rf docs
 cp -r _build docs
 [ -e docs/html ] && rm docs/html
 touch docs/.nojekyll
-echo "docs.odoopbx.com" > docs/CNAME
+#echo "docs.odoopbx.com" > docs/CNAME
 git add docs
 git commit docs -m "Docs updated"
 git push
