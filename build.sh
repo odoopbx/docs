4457440b0a3f78e6c30ffebf653384384bcd94c5
litnimax <litnimaxster@gmail.com>
2023-03-03 17:27:13 +0300

    python3 fix

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 rm -rf $DIR/_build
-python3.10 -m sphinx -t html $DIR $DIR/_build
+python3 -m sphinx -t html $DIR $DIR/_build
 cd $DIR/_build
 # Create a link for sphinx-serve
 ln -s . $DIR/_build/html
